6a90d179735c4660220d875253b2080a46db77b7
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-05-01 01:48:59 -0400

    adding all need custom components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './home-page';
+import App from './pages/home-page';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
